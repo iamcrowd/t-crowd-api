73ea4fdaeac38c9d64d532940d776233f61aa64d
German Braun <gab@gab.gab>
2019-11-19 10:22:49 -0300

    php code for invoking t-crowd

--- api/satisfiable.php
+++ api/satisfiable.php
@@ -1,4 +1,23 @@
 <?php
-$rta = "Hola Mundo";
-print_r($rta);
+
+namespace Tcrowd\api;
+
+include("config.php");
+include("connector.php");
+
+$reasoner = 'NuSMV';
+if (array_key_exists('reasoner',$_REQUEST)){
+    $reasoner = $_REQUEST['reasoner'];
+}
+
+if ( ! array_key_exists('json', $_POST)){
+    echo "I can not find any \"json\" parameter :-(";
+}else{
+
+    $sat = "";
+    $sat = new Sat();
+    $ans = $sat->check_sat($_POST['json'], $reasoner);
+    print_r($ans);
+    
+}
 ?>
