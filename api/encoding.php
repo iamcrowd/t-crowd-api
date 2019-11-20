5a3fef764b8b8114c0a14219dba6cf44f798a4c9
German Braun <gab@gab.gab>
2019-11-20 15:18:58 -0300

    add todos and generating html output backing from ajax call

--- api/encoding.php
+++ api/encoding.php
@@ -7,6 +7,9 @@ include("../src/encode.php");
 
 use Tcrowd\src\Encode;
 
+/**
+@// TODO: return link or result must be refactored.
+*/
 $formal = 'tdllitefpx';
 if (array_key_exists('formal',$_REQUEST)){
     $formal = $_REQUEST['formal'];
@@ -17,8 +20,11 @@ if ( ! array_key_exists('json', $_POST)){
 }else{
 
     $encoding = "";
+    $command = "";
     $encoding = new Encode();
     $ans = $encoding->encode($_POST['json'], $formal);
+    $command .= "latex2html " . "./tdl2TDLLiteFPX_tbox.tex";
+    exec($command);
     print_r($ans);
 
 }
